c223bfb153cf32860bfd4cd2a21685f2b144bbc4
mdamir-coder <mdamir2893@gmail.com>
2026-08-16 13:25:49 +0530

    Time: 10 ms (3.49%) | Memory: 31.4 MB (7.16%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
-        map<int, int> mpp;
+        unordered_map<int, int> mpp;
         for(int i=0; i<n; i++){
             mpp[nums[i]]++;
         }
