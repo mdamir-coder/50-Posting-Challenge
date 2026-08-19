88b8dd5a2379ee985d1cdba09e1f195baa36dd68
mdamir-coder <mdamir2893@gmail.com>
2026-08-19 15:04:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (43.89%) - LeetSync

---
+++ 326-power-of-three/power-of-three.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n > 0) {
+            if (1162261467 % n == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+        return false;
+    }
+    
+};
