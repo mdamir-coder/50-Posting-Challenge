c28ddd562da81674501fdfc91a0e80371ff8ae95
mdamir-coder <mdamir2893@gmail.com>
2026-08-24 19:05:41 +0530

    Time: 71 ms (77.58%) | Memory: 89 MB (49.26%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,32 +1,23 @@
-#include <vector>
-#include <unordered_set>
 #include <algorithm>
+#include <unordered_set>
+#include <vector>
 
 class Solution {
 public:
     int longestConsecutive(std::vector<int>& nums) {
-        // Step 1: Insert all numbers into an unordered set
-        std::unordered_set<int> numSet(nums.begin(), nums.end());
-        int longestSequence = 0;
-
-        // Step 2: Iterate through each unique number
-        for (int num : numSet) {
-            // Check if 'num' is the starting point of a sequence
-            if (numSet.find(num - 1) == numSet.end()) {
-                int currentNum = num;
-                int currentSequence = 1;
-
-                // Expand the sequence forward
-                while (numSet.find(currentNum + 1) != numSet.end()) {
-                    currentNum += 1;
-                    currentSequence += 1;
+        unordered_set<int> numset(nums.begin(), nums.end());
+        int longseq = 0;
+        for (int num : numset) {
+            if (numset.find(num - 1) == numset.end()) {
+                int currentnum = num;
+                int currentseq = 1;
+                while (numset.find(currentnum + 1) != numset.end()) {
+                    currentnum += 1;
+                    currentseq += 1;
                 }
-
-                // Update the maximum sequence length found
-                longestSequence = std::max(longestSequence, currentSequence);
+                longseq = max(currentseq, longseq);
             }
         }
-
-        return longestSequence;
+        return longseq;
     }
 };
